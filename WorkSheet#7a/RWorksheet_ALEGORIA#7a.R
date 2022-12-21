c1e1b9a27356f60117434ff977a9860da24a7cb4
Kirby Clarence <kirbyalegoria@gmail.com.com>
2022-12-21 17:01:03 +0630

    first commit

---
+++ WorkSheet#7a/RWorksheet_ALEGORIA#7a.R
@@ -0,0 +1,231 @@
+#-------------------------------------------------------------------------------
+#                                 Worksheet7a
+#-------------------------------------------------------------------------------
+
+
+install.packages("Hmisc")
+install.packages("pastecs")
+
+#1. Create a data frame for the table below. Figure 1: Student Scores
+
+    student <- seq(1:10)
+    preTest <- c(55,54,47,57,51,61,57,54,63,58)
+    postTest <- c(61,60,56,63,56,63,59,56,62,61)
+
+    DataFr <- data.frame(student,preTest,postTest)
+    DataFr
+
+#a. Compute the descriptive statistics using different packages 
+#   (Hmisc and pastecs). Write the codes and its result.
+
+library(Hmisc)
+library(pastecs)
+
+    describe(DF)
+
+    stat.desc(DF)
+
+    
+#2. The Department of Agriculture was studying the effects of several levels of 
+#   a fertilizer on the growth of a plant. For some analyses, it might be useful 
+#   to convert the fertilizer levels to an ordered factor.
+
+    DepartmentofAgriculture <- c(10,10,10,20,20,50,10,
+                                 20,10,50,20,50,20,10)
+
+    #a. Write the codes and describe the result.
+    
+    Order <- sort(DepartmentofAgriculture, decreasing = FALSE)
+    Order
+    
+    #Based on the result, the fertilizer levels were sorted from the lowest 
+    #to the highest value.
+    
+    
+#3. Abdul Hassan, president of Floor Coverings Unlimited, has asked you to study
+#   the exercise levels undertaken by 10 subjects were “l”, “n”, “n”, “i”, “l” ,
+#   “l”, “n”, “n”, “i”, “l” ; n=none, l=light, i=intense
+    
+#a. What is the best way to represent this in R?
+
+#   The best way to represent this is through dataframe.
+    
+    exercise_levels <- c("l","n","n","i","l","l","n","n","i","l")
+    
+    exerciselvl <- data.frame(exercise_levels)
+    exerciselvl
+    
+    exerlvl <- data.frame(
+      none = c("n", "n", "n", "n"),
+      light = c("l", "l", "l", "l"),
+      intense = c("i", "i", "NA", "NA")
+    )
+    exerlvl
+    
+    
+#4. Sample of 30 tax accountants from all the states and territories of 
+#   Australia and their individual state of origin is specified by a character 
+#   vector of state mnemonics as:
+
+    state <- c("tas", "sa", "qld", "nsw", "nsw", "nt", "wa", "wa", "qld",
+               "vic", "nsw", "vic", "qld", "qld", "sa", "tas", "sa", "nt",
+               "wa", "vic", "qld", "nsw", "nsw", "wa", "sa", "act", "nsw",
+               "vic", "vic", "act")
+    state 
+
+#a. Apply the factor function and factor level. Describe the results.
+
+    factorfunc <- function(state)
+    factorfunc
+    View(factorfunc)
+
+  
+#5. From #4 - continuation:
+  
+# Suppose we have the incomes of the same tax accountants in another vector 
+# (in suitably large units of money)
+
+    incomes <- c(60, 49, 40, 61, 64, 60, 59, 54, 62, 69, 70, 
+                 42, 56, 61, 61, 61, 58, 51, 48, 65, 49, 49, 
+                 41, 48, 52, 46, 59, 46, 58, 43)
+  
+  #a. Calculate the sample mean income for each state we can now use the special
+   #    function tapply():
+  
+    cal <- tapply(state, incomes, mean)
+    cal
+  
+  #b. Copy the results and interpret.
+  #    40 41 42 43 46 48 49 51 52 54 56 58 59 60 61 62 64 65 69 70 
+  #    NA NA NA NA NA NA NA NA NA NA NA NA NA NA NA NA NA NA NA NA
+  
+  
+#6. Calculate the standard errors of the state income means 
+    #(refer again to number 3)
+  
+    calcustd.n <- length(cal)
+    calculate.sd <- sd(cal)
+    calstd.se <- calculate.sd/sqrt(calcustd.n)
+    calstd.se
+  
+    #a. What is the standard error? Write the codes.
+    #   The result of the code above is NA.
+  
+   #b. Interpret the result.
+   #   The result is not available for the reason that the other objects are 
+   #   classified as character type, therefore it won't be able to get 
+   #   the standard error.
+    
+    
+#7. Use the titanic dataset.
+    data("Titanic")
+  
+    titanset<- data.frame(Titanic)
+    titanset
+    
+  #a. subset the titatic dataset of those who survived and not survived. 
+  #   Show the codes and its result.
+  
+        Sbset <- subset(titanset, select = "Survived")
+        Sbset
+        
+        
+#8. The data sets are about the breast cancer Wisconsin. The samples arrive 
+#   periodically as Dr. Wolberg reports his clinical cases. The database 
+#   therefore reflects this chronological grouping of the data. You can create 
+#   this dataset in Microsoft Excel.
+  
+  #a. describe what is the dataset all about.
+  #   The dataset is all about Breast Cancer Wisconsin.
+  
+  #b. Import the data from MS Excel. Copy the codes.
+  
+library("readxl")
+  DATA <- read_excel("C:/Users/acer/Documents/BSIT-2A/WorkSheet#7a/Breast_Cancer.xlsx")
+  DATA
+  
+  #c. Compute the descriptive statistics using different packages. 
+  #   Find the values of:
+  
+  #c.1 Standard error of the mean for clump thickness.
+       clump <- length(DATA$`CL. thickness`)
+       clump1 <- sd(DATA$`CL. thickness`)
+       clump2 <- clump1/sqrt(DATA$`CL. thickness`)
+       clump2
+  
+  #c.2 Coefficient of variability for Marginal Adhesion.
+       COV <- sd(DATA$`Marg. Adhesion`) / mean(DATA$`Marg. Adhesion`)* 100
+       COV
+  
+  #c.3 Number of null values of Bare Nuclei.
+       null_values <- subset(DATA,`Bare. Nuclei` == "NA")
+  
+  #c.4 Mean and standard deviation for Bland Chromatin
+       mean(DATA$`Bl. Cromatin`)
+       sd(DATA$`Bl. Cromatin`)
+  
+  #c.5 Confidence interval of the mean for Uniformity of Cell Shape
+  
+     #Calculate the mean
+       confInterMean <- mean(DATA$`Cell Shape`)
+       confInterMean
+  
+     #Calculate the standard error of the mean
+       stdErrMean <- length(DATA$`Cell Shape`)
+       stdB <- sd(DATA$`Cell Shape`)
+       result <- stdB/sqrt(stdErrMean)
+       result
+  
+     #Find the t-score that corresponds to the confidence level
+       tscore = 0.05
+       num1 = stdErrMean - 1
+       num2 = qt(p = tscore/ 2, df = num1,lower.tail = F)
+       num2
+  
+     #Constructing the confidence interval
+       num3 <- num2 * num1
+  
+     #LOWER
+       num4 <- stdErrMean - num3
+  
+     #UPPER
+       num5 <- stdErrMean + num3
+  
+       num6 <- c(num4,num5)
+       num6
+  
+     #d. How many attributes?
+         attributes(DATA)
+  
+     #e. Find the percentage of respondents who are malignant. 
+     #   Interpret the results.
+         percResp <- subset(DATA, Class == "maligant")
+         percResp
+  
+     #There 17 respondents who are malignant.
+     #And there are total of 49 respondent.
+  
+     #Getting the percentage
+         percenTage <- 17 / 49 * 100
+         percenTage
+  
+#9. Export the data abalone to the Microsoft excel file. Copy the codes.
+  
+        install.packages("AppliedPredictiveModeling")
+  
+library("AppliedPredictiveModeling")
+  
+        data("abalone")
+        View(abalone)
+        head(abalone)
+        summary(abalone)
+  
+        #Exporting the data abalone to the Microsoft excel file.
+          install.packages("xlsxjars")
+  
+library(xlsx)
+  write.xlsx("abalone","C:/Users/acer/Documents/BSIT-2A/WorkSheet#7a/abalone.xlsx")
+
+  
+  
+  
